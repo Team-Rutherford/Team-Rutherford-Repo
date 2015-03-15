fea470380be75f1c153261e33aa492ef10be0cf0
TzvetanIG <cecoig@mail.bg>
2015-03-15 23:54:50 +0200

    Added MongoDB

---
+++ MarketSystem/MarketSystem/MongoDbDatabase/MongoDbManager.cs
@@ -0,0 +1,22 @@
+﻿namespace MongoDbDatabase
+{
+    using MongoDB.Bson;
+    using MongoDB.Driver;
+    using MongoDB.Driver.Builders;
+    using MongoDB.Driver.GridFS;
+    using MongoDB.Driver.Linq;
+
+    public static class MongoDbManager
+    {
+        public static void AddSalesProductReportsFromFilesToDatabase(string directoryName)
+        {
+            var reports = Json.GetProductReportsFromDirectory(directoryName);
+            var db = new MongoDb("MarketSystem");
+            db.CreateCollection<ProductReportClass>("SaleProducts");
+            foreach (var report in reports)
+            {
+                db.Collections["SaleProducts"].Insert(report);
+            }
+        }
+    }
+}

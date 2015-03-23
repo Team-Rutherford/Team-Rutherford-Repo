413bd3555099c29d5cb3fac3a86c7f96dec28cc7
Jazastry <yogib@mail.bg>
2015-03-23 16:51:22 +0200

    Cleaned All Files and Folders.

--- MarketSystem/MarketSystem/OracleDatabase/OracleDb.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'D:\SoftUni\DatabaseApplications\Teamwork\Team-Rutherford-Repo\MarketSolution\MarketSystem\MarketSystem\OracleDatabase\OracleDb.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
